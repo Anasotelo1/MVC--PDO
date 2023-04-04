781dddd235860c5038cc7a5f172bc03d650df3f3
Ana Sotelo <Sotelocardenasa@gmail.com>
2023-04-04 10:34:17 -0500

    Metodo listar terminado en Curso

--- models/Curso.php
+++ models/Curso.php
@@ -7,17 +7,59 @@ require_once "./Conexion.php";
 class Curso extends Conexion{
 
   //Objeto que almacena la conexion que viene desde el padre(Conexion)
+  // y la compartira con los metodos (CRUD+)
   private $accesoBD;
 
-  //Constructor 
+  //Constructor, INICIALIZAR            
+  // Valor de retorno de la funccion sera asignada a accesoBD 
   public fuction __CONSTRUCT(){
-
+    $this->accesoBD = parent::getConexion(); 
   }
 
   //Metodo listar cursos
   public function listarCursos(){
-    
+    try{
+      // 1. Preparamos la consulya
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_listar()");
+      //2. Ejecutamos la consulta
+      $consulta->execute();
+      //3. Devolvemos el resultado (array asociativo)
+      return $consulta->fecthAll(PDO::FETCH_ASSOC);
+    }
+    catch(Excepction $e){
+      die($e->getMessage());
+    }
+
+  }
+
+  public function registrarCurso(){
+    try{
+       
+    }
+    catch(Excepction $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function eliminarCurso(){
+    try{
+
+    }
+    catch(Excepction $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function actualizarCurso(){
+    try{
+
+    }
+    catch(Excepction $e){
+      die($e->getMessage());
+    }
   }
 
 
 }
+
+?>
