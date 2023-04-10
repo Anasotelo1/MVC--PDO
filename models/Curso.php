c61e3ed6295e58418f505f9c613321ff0858bd30
Ana Sotelo <1328493@senati.pe>
2023-04-10 09:08:47 -0500

    Modelo y controlador de obtener Curso terminado

--- models/Curso.php
+++ models/Curso.php
@@ -63,6 +63,18 @@ class Curso extends Conexion{
     }
   }
 
+  public function getCurso($idcurso = 0){
+    try{
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_recuperar_id(?)");
+      $consulta->execute(array($idcurso));
+      //Retornar el registro encontrado
+      return $consulta->fetch(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+      die($e->getMessage);
+    }
+  }
+
   public function actualizarCurso(){
     try{
 
@@ -71,6 +83,8 @@ class Curso extends Conexion{
       die($e->getMessage());
     }
   }
+
+  
 
 
 }
