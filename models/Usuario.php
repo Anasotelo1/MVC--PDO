b12f3029f22f00fe15b242810c435b9cccda5578
Ana Sotelo <1328493@senati.pe>
2023-04-10 15:12:17 -0500

    Poceso LOGIN validado

---
+++ models/Usuario.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Usuario extends Conexion{
+
+  private $accesoBD;  //Conexion
+
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
+  }
+
+  public function iniciarSesion($nombreUsuario = ""){
+    try {
+      $consulta = $this->accesoBD->prepare("CALL spu_usuarios_login(?)");
+      $consulta->execute(array($nombreUsuario));
+      return $consulta->fetch(PDO::FETCH_ASSOC);
+
+    } catch(Exception $e) {
+      die($e->getMesagge());
+    }
+  }
+
+  public function registrarUsuario(){
+    try {
+      
+    } catch(Exception $e) {
+      die($e->getMesagge());
+    }
+  }
+
+  public function eliminarUsuario(){
+    try {
+      
+    } catch(Exception $e) {
+      die($e->getMesagge());
+    }
+  }
+
+  public function listarUsuarios(){
+    try {
+      
+    } catch(Exception $e) {
+      die($e->getMesagge());
+    }
+  }
+}
